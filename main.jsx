82daaf3f45afd7dd98961d51bd3accbe693f3266
OlgaGladyr <GladyrOlga@gmail.com>
2026-07-22 14:10:58 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AIPlanner from "./AIPlanner.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AIPlanner />
+  </React.StrictMode>
+);
